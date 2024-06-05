40bf9cdf39ab272a125c254d107ca0c12945a469
Gianni <117100331+SW364@users.noreply.github.com>
2024-06-04 18:01:27 -0600

    Add files via upload

---
+++ Web/models/session.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Session = sequelize.define('Session', {
+  id_session: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  id_user: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'users',
+      key: 'user_id'
+    }
+  },
+  id_progress: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'progress',
+      key: 'id_progress'
+    }
+  },
+  duration_time: DataTypes.TIME,
+  start_date_time: DataTypes.DATE,
+  finish_date_time: DataTypes.DATE,
+  status_s: DataTypes.BOOLEAN
+}, {
+  tableName: 'sessions',
+  timestamps: false
+});
+
+
+module.exports = Session;

39668a0d2694c024234cbabcf04aabfab4e77a48
Bryan De La O <83720669+Spring1077@users.noreply.github.com>
2024-06-04 23:57:44 -0600

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,44 +7,45 @@ require('dotenv').config();
 
 const app = express();
 
-
-app.get('/',(req,res)=>{
-    res.send("backend funcionando")
-
-})
-
+// Define the whitelist
 const whitelist = [
-    'https://life-developers.vercel.app/', // Reemplaza con la URL de tu frontend en Vercel
-    'http://localhost:3000' // Mantén localhost para desarrollo local
-  ];
-  
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true, // Permitir cookies y encabezados de autorización
-  };
-  
-  app.use(cors(corsOptions));
+    'https://life-developers.vercel.app', // URL of your frontend in Vercel
+    'http://localhost:3000' // Keep localhost for local development
+];
 
-//app.use(cors());
+// Configure CORS options
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // Allow cookies and authorization headers
+};
+
+// Apply the CORS middleware
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/User', userRoutes)
-// Sincroniza la base de datos
+// Define your routes
+app.get('/', (req, res) => {
+    res.send("backend funcionando");
+});
+
+app.use('/User', userRoutes);
+
+// Sync the database
 sequelize.sync().then(() => {
     console.log('Database synced');
 }).catch(err => {
     console.error('Error syncing database:', err);
 });
 
-
-
+// Start the server
 app.listen(4000, () => {
     console.log('Backend server listening on port 4000');
 });
